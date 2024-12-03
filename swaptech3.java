23be91cacbf10bfb34d96e9fbc55dd21610c503d
mounikapujari <mounikapujari2005@gmail.com>
2024-12-03 12:26:51 +0530

    Add files via upload

---
+++ swaptech3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class swaptech3{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        a=a*b;
+        b=a/b;
+        a=a/b;
+        String res=String.format("after Swapping\n boy=%d \n girl=%d",a,b);
+        System.out.println(res);
+    }
+}
